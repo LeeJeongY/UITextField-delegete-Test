f734aa03da06c5297b418559c837a89dcb5e9183
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-03-22 15:50:17 +0900

    change

--- UITextField delegete Test/ViewController.swift
+++ UITextField delegete Test/ViewController.swift
@@ -13,13 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textfield.placeholder = "입력을 하세요"
+        textfield.clearButtonMode = UITextFieldViewMode.whileEditing
+        textfield.borderStyle = UITextBorderStyle.line
     }
 
+    @IBOutlet weak var hellolabel: UILabel!
+    
+    @IBOutlet weak var textfield: UITextField!
+    
+    
+    @IBAction func Button(_ sender: Any) {
+        hellolabel.text = "Hello " + textfield.text!
+        textfield.text=""
+        
+        textfield.resignFirstResponder() //키패드를 내림
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+    
+}
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textfield.resignFirstResponder()
     }
-
-
 }
 
